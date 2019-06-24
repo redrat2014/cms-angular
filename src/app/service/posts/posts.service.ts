4a5f7101a7a0ff0cf6d407ec8101959be32a1253
redrat76 <redrat76@redrat76s-iMac.local>
2019-06-24 14:14:48 -0300

    Add Post Service

--- src/app/service/posts/posts.service.ts
+++ src/app/service/posts/posts.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { map } from "rxjs/operators";
+
+export interface Post {
+  title: '',
+  url: ''
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  getMenus() {
+    return this.afs.collection('posts').snapshotChanges()
+    .pipe(map( post => {
+        return post.map ( a => {
+          const data = a.payload.doc.data() as Post;
+          const id = a.payload.doc.id;
+          return {id, ...data};
+        })
+      }))
+  }
+  addMenu(post: Post) {
+    this.afs.collection('posts').add(post);
+  }
+  deleteMenu(postId) {
+    this.afs.doc('posts/'+postId).delete();
+  }
+  updateMenu(postId, post: Post) {
+    this.afs.doc('posts/'+postId).update(post);
+  }
 }

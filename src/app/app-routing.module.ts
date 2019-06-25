db65814b9a3cb63cdf6c330ae1f42663dc3242ae
redrat76 <redrat76@redrat76s-iMac.local>
2019-06-24 23:51:32 -0300

    Add front side routing and layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { AdminGuard } from './guards/admin.guard';
 
 const routes: Routes = [
-  {path: '', loadChildren: () => import('./front-page/front-page.module').then(p => p.FrontPageModule)},
+  {path: 'home', loadChildren: () => import('./front-page/front-page.module').then(p => p.FrontPageModule)},
   {path: 'login', component: LoginPageComponent},
-  {path: 'admin', loadChildren: () => import('./admin-page/admin-page.module').then(m => m.AdminPageModule), canActivate: [AdminGuard]}
+  {path: 'admin', loadChildren: () => import('./admin-page/admin-page.module').then(m => m.AdminPageModule), canActivate: [AdminGuard]},
+  {path: '**', redirectTo: 'home'}
 ]
 
 @NgModule({
--- src/app/service/menus/menus.service.ts
+++ src/app/service/menus/menus.service.ts
@@ -24,6 +24,18 @@ export class MenusService {
         })
       }))
   }
+
+  getConditionalMenus(field: string, condition: any, value: string) {
+    return this.afs.collection('menus', ref => ref.where(field, condition, value)).snapshotChanges()
+    .pipe(map( menu => {
+        return menu.map ( a => {
+          const data = a.payload.doc.data() as Menu;
+          const id = a.payload.doc.id;
+          return {id, ...data};
+        })
+      }))
+  }
+
   addMenu(menu: Menu) {
     this.afs.collection('menus').add(menu);
   }

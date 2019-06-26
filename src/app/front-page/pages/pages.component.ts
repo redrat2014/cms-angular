c84027d3a359ff1abcccfacab0eed88cf5307bed
redrat76 <redrat76@redrat76s-iMac.local>
2019-06-26 13:50:08 -0300

    Add style front-end

--- src/app/front-page/pages/pages.component.ts
+++ src/app/front-page/pages/pages.component.ts
@@ -1,7 +1,10 @@
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MenusService, Menu } from './../../service/menus/menus.service';
 import { PostsService } from './../../service/posts/posts.service';
+import { User } from './../../providers/user';
+import { AfService } from './../../providers/af.service';
 
 @Component({
   selector: 'app-pages',
@@ -12,8 +15,9 @@ export class PagesComponent implements OnInit {
 
   menu: any;
   postsList: any;
+  user: User ;
 
-  constructor(private route: ActivatedRoute, private menus: MenusService, private posts: PostsService) { 
+  constructor(private route: ActivatedRoute, private menus: MenusService, private posts: PostsService, public afService: AfService) { 
     this.route.params.subscribe( params => {
       console.log(params);
       this.menus.getConditionalMenus('url', '==', params.url).subscribe( menus => {
@@ -30,6 +34,7 @@ export class PagesComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.afService.user$.subscribe(user => this.user = user)
   }
 
 }
